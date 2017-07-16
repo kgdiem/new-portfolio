ad49729db0f832d11abc6fa959d6279e567b87a8
Kevin Diem <kg.diem@gmail.com>
2017-07-16 00:23:56 +0000

    compile index.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -121,7 +121,7 @@ function appendProjects(projects) {
 function projectNode(project) {
   var p = document.createElement('div');
 
-  var description = project.description ? project.description : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+  var description = project.desc ? project.desc : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
 
   p.classList.add('col-12');
   p.classList.add('projects');
